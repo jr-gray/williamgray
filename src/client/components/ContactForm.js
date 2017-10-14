54b1f5632dcbcea6a81c30b10d5ce0db48e6aeb6
JR Gray <jrgrayy@gmail.com>
2017-10-14 15:54:53 -0700

    Start email portion of contact form

--- src/client/components/ContactForm.js
+++ src/client/components/ContactForm.js
@@ -3,21 +3,44 @@ import React, { Component } from 'react';
 export default class ContactForm extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(e) {
+    let temp = e.target.name;
+    this.setState({
+      [temp]: e.target.value
+    })
+  }
+
+  handleSubmit() {
+    console.log('name:', this.state.name);
+    console.log('email:', this.state.email);
+    console.log('message:', this.state.message);
+    this.setState({
+      name: '',
+      email: '',
+      message: ''
+    }, () => { alert('Message sent!'); })
   }
 
   render() {
     return (
       <div>
         <h2>Contact</h2>
-        <input type="text"/>
-        <input type="text"/>
-        <select>
-          <option value="">Inquiry</option>
-          <option value="">General</option>
-          <option value="">Career Opportunity</option>
-        </select>
-        <textarea rows="5" cols="20"></textarea>
+        <span>Name:</span>
+        <input type="text" name="name" value={this.state.name} onChange={this.handleInput}/>
+        <span>Email:</span>
+        <input type="text" name="email" value={this.state.email} onChange={this.handleInput}/>
+        <textarea placeholder="Enter your message here..." value={this.state.message} 
+          name="message" rows="5" cols="20" onChange={this.handleInput}></textarea>
+        <button className="btn btn-primary btn-block" type="Submit" onClick={this.handleSubmit}>Submit</button>
       </div>
     )
   }
