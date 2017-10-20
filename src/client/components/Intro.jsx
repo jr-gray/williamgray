e59cb658fe58b5d3def94b7019c38ced24e3afdd
JR Gray <jrgrayy@gmail.com>
2017-10-19 17:35:34 -0700

    Style intro section

--- src/client/components/Intro.jsx
+++ src/client/components/Intro.jsx
@@ -3,12 +3,8 @@ import React, { Component } from 'react';
 export default class Intro extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row align-self-center">
-          <div className="col text-center">
-            <h1>Hi, I'm William Gray</h1>
-          </div>
-        </div>
+      <div className="intro-section text-center">
+        <h1 className="top-header">Hi, I'm William Gray</h1>
       </div>
     )
   }
