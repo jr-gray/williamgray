859a85f20d484715e28c5901689789e3f1064e4f
JR Gray <jrgrayy@gmail.com>
2017-10-22 20:52:10 -0700

    Add caret animation

--- src/client/components/Intro.jsx
+++ src/client/components/Intro.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 export default class Intro extends Component {
   render() {
     return (
-      <div className="intro-section text-center">
-        <h1 className="top-header">Hi, I'm William Gray</h1>
+      <div className="intro-section text-center fill">
+        <div className="container">
+          <h1 className="top-header">Hi, I'm William Gray</h1>
+          <hr className="intro-line"/>
+          <span className="homepage-caret animated bounceInUp" style={{'animationDuration': '1.7s'}}></span>
+        </div>
       </div>
     )
   }
