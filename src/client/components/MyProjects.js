486a3b974d7df0bf7ff608fa614e9ee8846219f6
JR Gray <jrgrayy@gmail.com>
2017-10-29 16:33:11 -0700

    Add github and web logos

--- src/client/components/MyProjects.js
+++ src/client/components/MyProjects.js
@@ -13,23 +13,28 @@ export default class MyProjects extends Component {
         <div className="row">
           <div className="col-md-6">
             <div className="profileCard">
+            <a href="http://www.woolfey.com">
               <WoolfeyCarousel />
               <div className="profileContainer">
-                <h2 className="top-header">Woolfey</h2>
-                <p>Cryptocurrency trading simulator with machine learning models.</p>
-                <a href="http://www.woolfey.com"><button className="cardButton">woolfey.com</button></a>
+                <h2 className="card-header">Woolfey</h2>
+                <p className="card-info">Cryptocurrency trading simulator with machine learning models.</p>
+                <img className="project-icon" src="/images/globe.png" height="30" width="30" /><span className="project-link"> woolfey.com</span>
+                <img className="project-icon" src="/images/github-logo.svg" height="30" width="30" /><span className="project-link"> jr-gray/woolfey</span>
               </div>
+            </a>
             </div>
           </div>
 
           <div className="col-md-6">
             <div className="profileCard">
+            <a href="https://www.github.com/jr-gray/syrup">
               <SyrupCarousel />
               <div className="profileContainer">
-                <h2 className="top-header">Syrup</h2>
-                <p>Facial Recognition dating app.</p>
-                <a href="https://www.github.com/jr-gray/syrup"><button className="cardButton">Syrup code</button></a>
+                <h2 className="card-header">Syrup</h2>
+                <p className="card-info">Facial Recognition dating app.</p>
+                <img src="/images/github-logo.svg" height="30" width="30" /><span className="project-link"> jr-gray/syrup</span>
               </div>
+            </a>
             </div>
           </div>
         </div>
